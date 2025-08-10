9ddabe88b4aec64775942221064cc7701a2e6e9e
Tiziano Crisci <tiziano.tixchim@gmail.com>
2025-08-10 10:32:54 +0200

    -

--- check-arch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-arch=$(uname -m)
-if [[ "$arch" == "arm64" || "$arch" == "aarch64" ]]; then
-  echo "Running on ARM64 architecture ($arch)."
-else
-  echo "Running on $arch architecture."
-fi
