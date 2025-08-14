d36b045a05225435bd86fb3856c55ce034debd8f
Tiziano Crisci <tiziano.tixchim@gmail.com>
2025-08-15 00:11:20 +0200

    -

--- Tix.IBMMQ.Bridge.IntegrationTests/Services/MqTlsEndToEndTests.cs
+++ Tix.IBMMQ.Bridge.IntegrationTests/Services/MqTlsEndToEndTests.cs
@@ -1,27 +1,21 @@
 using System.Collections;
 using IBM.WMQ;
 using Xunit;
-using Xunit.Abstractions;
 
 namespace Tix.IBMMQ.Bridge.IntegrationTests.Services;
 
-public class MqTlsEndToEndTests(MqTlsFixture fx, ITestOutputHelper logger) : IClassFixture<MqTlsFixture>
+public class MqTlsEndToEndTests
 {
-    [Fact(DisplayName = "Put/Get via TLS (managed .NET client)")]
+    [Fact]
     public void PutGet_Tls_Works()
     {
-        var webPort = fx.Container.GetMappedPublicPort(9443);
-        logger.WriteLine($"MQ Console → https://localhost:{webPort}/ibmmq/console/");
-        
         var props = new Hashtable
         {
-            { MQC.HOST_NAME_PROPERTY, fx.Host },
-            { MQC.PORT_PROPERTY, fx.Port },
+            { MQC.HOST_NAME_PROPERTY, "2.tcp.eu.ngrok.io" },
+            { MQC.PORT_PROPERTY, 13149 },
             { MQC.CHANNEL_PROPERTY, "APP.TLS.SVRCONN" },
             { MQC.TRANSPORT_PROPERTY, MQC.TRANSPORT_MQSERIES_CLIENT },
             { MQC.SSL_CIPHER_SPEC_PROPERTY, "TLS_RSA_WITH_AES_128_CBC_SHA256" }
-            //{ MQC.SSL_CERT_STORE_PROPERTY, "*USER" } // usa store CurrentUser
-            // Per TLS 1.3 su client 9.4+ e QM abilitato: "ANY_TLS13_OR_HIGHER"
         };
 
         using var qmgr = new MQQueueManager("QM1", props);
