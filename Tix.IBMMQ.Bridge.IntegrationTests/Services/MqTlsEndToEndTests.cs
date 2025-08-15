b4a208e7987db0b23888a3cfd8b5b349dda61421
Tiziano Crisci <tiziano.tixchim@gmail.com>
2025-08-15 09:58:27 +0200

    -

--- Tix.IBMMQ.Bridge.IntegrationTests/Services/MqTlsEndToEndTests.cs
+++ Tix.IBMMQ.Bridge.IntegrationTests/Services/MqTlsEndToEndTests.cs
@@ -18,10 +18,13 @@ public class MqTlsEndToEndTests
             { MQC.HOST_NAME_PROPERTY, "2.tcp.eu.ngrok.io" },
             { MQC.PORT_PROPERTY, 17083 },
             { MQC.CHANNEL_PROPERTY, "APP.TLS.SVRCONN" },
-            { MQC.TRANSPORT_PROPERTY, MQC.TRANSPORT_MQSERIES_CLIENT },
+            { MQC.TRANSPORT_PROPERTY, MQC.TRANSPORT_MQSERIES_MANAGED },
             
             //I'm not sure this is the way on linux, on other cases i need to create a store and and ca cert
             //{ MQC.SSL_CERT_STORE_PROPERTY, certPath },
+            
+            //I'm not sure if we need to use SSL_CIPHER_SUITE_PROPERTY or SSL_CIPHER_SPEC_PROPERTY
+            //{ MQC.SSL_CIPHER_SUITE_PROPERTY, "TLS_RSA_WITH_AES_128_CBC_SHA256" }
             
             { MQC.SSL_CIPHER_SPEC_PROPERTY, "TLS_RSA_WITH_AES_128_CBC_SHA256" }
         };
